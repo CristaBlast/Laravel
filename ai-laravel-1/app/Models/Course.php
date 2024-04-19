411e71453a46358393e8f38026a9aa346998434d
CristaBlast <72558559+CristaBlast@users.noreply.github.com>
2024-04-19 12:15:07 +0100

    ai 1

---
+++ ai-laravel-1/app/Models/Course.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'abbreviation',
+        'name',
+        'name_pt',
+        'type',
+        'semesters',
+        'ECTS',
+        'places',
+        'contact',
+        'objectives',
+        'objectives_pt',
+        ];
+
+        public $timestamps = false;
+
+        protected $primaryKey = 'abbreviation';
+        public $incrementing = false;
+        protected $keyType = 'string';
+}
